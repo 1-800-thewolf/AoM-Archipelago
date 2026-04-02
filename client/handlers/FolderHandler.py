29e0a964ff03db524c944ddf9880c2381f630a6f
Phil <thephilipwolfe@gmail.com>
2026-04-02 15:54:17 -0700

    Cleaned up unneeded directories and files

--- client/handlers/FolderHandler.py
+++
@@ -1,8 +0,0 @@
-class FolderHandler:
-    _user_folder: str
-    
-    def __init__(self):
-        pass
-    
-    def set_user_folder(self, user_folder: str):
-        self._user_folder = user_folder
